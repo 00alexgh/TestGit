51bc38b294080bb0846e980db539bf2da4caf68d
Alex GH <105998094+AlexGarH1@users.noreply.github.com>
2023-08-08 11:59:31 -0600

    <First commit in Master>

--- TestGithub/Controllers/HomeController.cs
+++ TestGithub/Controllers/HomeController.cs
@@ -10,22 +10,26 @@ namespace TestGithub.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
+            int a = 0;
             _logger = logger;
         }
 
         public IActionResult Index()
         {
+            int b = 0;
             return View();
         }
 
         public IActionResult Privacy()
         {
+            int c = 0;
             return View();
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            int d = 0;
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
